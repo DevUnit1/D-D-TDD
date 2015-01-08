f5c3a0f6725cec7345432197d76d7291049d3add
Chris Morton <christopher_morton@hotmail.com>
2015-01-07 19:48:13 -0500

    Name

---
+++ TDD/TDDTests/CharacterSheetTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evercraft;
+
+namespace TDDTests
+{
+    [TestClass]
+    public class CharacterSheetTests
+    {
+        [TestMethod]
+        public void CharacterSheetHasName()
+        {
+            var character = new CharacterSheet("Awesome");
+
+            Assert.AreEqual("Awesome", character.Name);
+        }
+    }
+}
